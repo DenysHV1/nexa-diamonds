97f2ba1c3a383a587334b9ce02867e9f0f3172c5
DenVik <denyagarkusha2@gmail.com>
2025-04-05 13:33:06 +0200

    fix

--- global.styles.js
+++ global.styles.js
@@ -1,8 +1,15 @@
 const variablesStyles = '@import "./src/css/common/_vars.scss"';
+const breakpointsStyles = '@import "./src/css/common/_breakpoints.scss"';
+const mixinsStyles = '@import "./src/css/common/_mixins.scss"';
 
 
 const styles = [
-  variablesStyles
+  variablesStyles,
+  breakpointsStyles,
+  //   colorsStyles,
+  mixinsStyles,
+  //   baseStyles,
+  // resetStyles,
 ];
 
 export const globalStylesOptions = styles.reduce((acc, i) => acc + i + ';', '');
